f58273afa7e8f97077c2b87a0dbbb4852819f50c
Vincent1101 <vincent@bestitmaster.com>
2019-07-31 13:37:40 +0800

    程式調整

--- ISTI_CityNavigation/handler/ChangeUserPwd.aspx.cs
+++ ISTI_CityNavigation/handler/ChangeUserPwd.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace ISTI_CityNavigation.handler
 {
